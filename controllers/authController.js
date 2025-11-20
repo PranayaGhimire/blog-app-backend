d6a63f0095c94d415d42d35156f5ce137c4e94fb
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-11-20 22:04:07 +0545

    New Changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,30 +4,42 @@ import jwt from "jsonwebtoken";
 
 export const register = async (req,res) => {
     const {email,password} = req.body;
-    const userExist = await User.findOne({email});
-    if (userExist)
-        return res.status(400).json({ message : "User already exist"});
-    const hashPassword = await bcrypt.hash(password,10);
-    const user = await User.create({...req.body,password:hashPassword});
-    const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"});
-    return res.status(201).json({
-        success:true,
-        message:"User registered successfully",
-        data:user,
-        token
-    });
+    try {
+        const userExist = await User.findOne({email});
+        if (userExist)
+            return res.status(400).json({ message : "User already exist"});
+        const hashPassword = await bcrypt.hash(password,10);
+        const user = await User.create({...req.body,password:hashPassword});
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"});
+        return res.status(201).json({
+            success:true,
+            message:"User registered successfully",
+            data:user,
+            token
+        });   
+    } catch (error) {
+        res.status(500).json({
+            message:error
+        })
+    }
 }
 
 export const login = async (req,res) => {
     const {email} = req.body;
-    const user = await User.findOne({email});
-    if (!user)
-        return res.status(400).json({message:"Invalid Credentials"});
-    const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"});
-    return res.status(201).json({
-        success:true,
-        message:"User logged in successfully",
-        data:user,
-        token
-    });        
+    try {
+        const user = await User.findOne({email});
+        if (!user)
+            return res.status(400).json({message:"Invalid Credentials"});
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"});
+        return res.status(201).json({
+            success:true,
+            message:"User logged in successfully",
+            data:user,
+            token
+        });    
+    } catch (error) {
+        res.status(500).json({
+            message:error
+        })
+    }       
 }
